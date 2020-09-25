2af4995b35e77391721be49bb94cbf74b6d185a4
andriyhakman <andrey.hakman@gmail.com>
2020-09-25 14:42:17 +0000

    Sample Code

--- js/index.js
+++ js/index.js
@@ -1,23 +1,39 @@
-const display = document.querySelector('.display');
+const calculatorResultField = document.querySelector(".calculator-result");
 
-document.querySelectorAll('.digits button')
-    .forEach( button => button.addEventListener('click', digitPressed));
+document
+  .querySelectorAll(".digits button")
+  .forEach((button) => button.addEventListener("click", digitPressed));
 
-function digitPressed(ev) {
-    const digit = ev.target.innerText;
-    display.value += digit;
+function digitPressed(event) {
+  const digit = event.target.innerText;
+  calculatorResultField.value += digit;
 }
 
-document.querySelectorAll('.opers button')
-    .forEach( button => button.addEventListener('click', operPressed));
+document
+  .querySelectorAll(".opers button")
+  .forEach((button) => button.addEventListener("click", operPressed));
 
-function operPressed(ev) {
-    const oper = ev.target.innerText;
-    display.value += oper;
+function operPressed(event) {
+  const oper = event.target.innerText;
+  const resultFieldValue = calculatorResultField.value;
+  const lastSymbol = resultFieldValue[resultFieldValue.length - 1];
+  //calculatorResultField.value += oper;
+  if (lastSymbol === 
 }
 
-document.querySelector('.eq').addEventListener('click', eqPressed);
+document.querySelector(".eq").addEventListener("click", eqPressed);
 
 function eqPressed() {
-    display.value = eval(display.value);
+  const result = eval(calculatorResultField.value);
+  if (result === Infinity) {
+    alert("Ви не можете ділити на 0");
+  } else {
+    calculatorResultField.value = result;
+  }
+}
+
+document.querySelector(".cl").addEventListener("click", acPressed);
+
+function acPressed() {
+  calculatorResultField.value = "";
 }
